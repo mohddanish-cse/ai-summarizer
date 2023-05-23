f8a75ddcc0e46bbfa204d0a0dd251943313ce7cf
Mohd Danish <mddanish.cse@gmail.com>
2023-05-23 12:41:55 +0530

    Updating the files

--- src/components/Demo.jsx
+++ src/components/Demo.jsx
@@ -13,7 +13,9 @@ const Demo = () => {
   
   return (
     <section className='mt-16 w-full max-w-xl'>
+
       {/* Search */}
+      
       <div className='flex flex-col w-full gap-2'>
         <form className='relative flex justify-center items-center'
           onSubmit={handleSubmit}>
